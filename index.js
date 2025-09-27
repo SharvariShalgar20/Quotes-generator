46e5d31a930dc93b5f06334eda43656cf118b050
SharvariShalgar20 <shalgarsharvari@gmail.com>
2025-09-27 19:23:40 +0530

    added some html text

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send(`hello!,search for quotes on ${port}/quotes`);
+  res.send(`<html>
+              <head>
+                <style>
+                  .quote{
+                    font-size:1.5rem;
+                    font-style:italic;
+                  }
+                </style>
+              </head>
+              <body>
+                <h1>Hello! Welcome to the page</h1>
+                <h2 class="quote">search for localhost:3000/quotes for the quotes!</h2>
+              </body>
+            
+            </html>`);
 })
 
 app.get('/quotes', (req, res) => {
